888eaab6a661214fe6f07253df04633b2528f08c
Abdullah <abdullah.abdulrahim@outlook.com>
2020-05-17 19:14:26 +0200

    add random-color-generator

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var randomColor = require('random-color');
 var chalk = require('chalk');
 var color = randomColor(0.5, 0.95);
 //console.log(color);
-console.log(color.hexString());
 console.log(
   chalk.hex(color.hexString())(
     `##############################
